1fea15e9c471f563a539679099f1e14ba27a9476
bgiobbe <bgiobbe@gmail.com>
2022-10-22 13:26:57 -0400

    Fetch 5 random users from the randomuser.me API and display their name, country, and avatar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api/1.4?results=5");
+    const data = await usersRequest.json();
+    //console.log(data);
+
+    // get array of users
+    const userResults = data.results;
+    //console.log(userResults);
+
+    displayUsers(userResults);
+};
+
+const displayUsers = function (users) {
+    randomFolks.innerHTML = "";
+
+    for (let user of users) {
+        const country = user.location.country;
+        const name = `${user.name.title} ${user.name.first} ${user.name.last}`;
+        const imageUrl = user.picture.medium;
+        
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src="${imageUrl}" alt="User avatar">
+        `;
+        randomFolks.append(userDiv);
+    };
+};
+
+getData();
+
